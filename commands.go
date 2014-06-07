22af098febfdac898dbde91f610d2141e6249a62
Spencer Judd <spencercjudd@gmail.com>
2014-06-06 23:41:38 -0400

    Merged commands.go with connection.go.

--- commands.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func (c *Connection) Join(ch string) {
-	c.SendString("JOIN " + ch + "\r\n")
-}
-
-func (c *Connection) Privmsg(who, msg string) {
-	c.SendString("PRIVMSG " + who + " :" + msg + "\r\n")
-}
--- connection.go
+++ connection.go
@@ -123,3 +123,11 @@ func (c *Connection) Connect(server, nick string) error {
 func (c *Connection) SendString(s string) {
 	c.write <- s
 }
+
+func (c *Connection) Join(ch string) {
+	c.SendString("JOIN " + ch + "\r\n")
+}
+
+func (c *Connection) Privmsg(who, msg string) {
+	c.SendString("PRIVMSG " + who + " :" + msg + "\r\n")
+}
