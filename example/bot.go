a71c151abeedf42e76a95f3a3bb9ace03586b0bb
Spencer Judd <spencercjudd@gmail.com>
2014-06-07 23:12:20 -0400

    Proper Go packaging.
    The package name is now `irc`, and the `main.go` file has been moved to
    `example/bot.go` and imports `github.com/scjudd/irc`.

--- example/bot.go
+++ example/bot.go
@@ -3,22 +3,24 @@ package main
 import (
 	"log"
 	"strings"
+
+	"github.com/scjudd/irc"
 )
 
 func main() {
-	c := NewConnection()
+	c := irc.NewConnection()
 
 	// Join #bot once handshake completes
-	c.RegisterHandler("001", func(msg *Message) {
+	c.RegisterHandler("001", func(msg *irc.Message) {
 		c.Join("#bot")
 	})
 
 	// Join an arbitrary channel on invitation
-	c.RegisterHandler("INVITE", func(msg *Message) {
+	c.RegisterHandler("INVITE", func(msg *irc.Message) {
 		c.Join(msg.Params[1])
 	})
 
-	c.RegisterHandler("PRIVMSG", func(msg *Message) {
+	c.RegisterHandler("PRIVMSG", func(msg *irc.Message) {
 		if strings.HasPrefix(msg.Params[1], "!") {
 			// Send raw command to server
 			c.SendRawMessage(msg.Params[1][1:] + "\r\n")
